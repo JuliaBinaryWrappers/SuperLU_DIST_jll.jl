1620b669221f3e700de670359647cbc7f2ed1c39
jlbuild <juliabuildbot@gmail.com>
2026-03-25 13:42:39 +0000

    SuperLU_DIST_jll build 8.2.2+0

--- src/SuperLU_DIST_jll.jl
+++ src/SuperLU_DIST_jll.jl
@@ -7,5 +7,5 @@ Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("SuperLU_DIST")
-JLLWrappers.@generate_main_file("SuperLU_DIST", UUID("9a1356b0-3c82-5da3-b77c-7c198e8bd7ab"))
+JLLWrappers.@generate_main_file("SuperLU_DIST", Base.UUID("9a1356b0-3c82-5da3-b77c-7c198e8bd7ab"))
 end  # module SuperLU_DIST_jll
